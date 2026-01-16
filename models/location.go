48595c169f86c2bb4768b8bcef1193a4c634f755
Xu Olivier <olivierxu36@gmail.com>
2026-01-16 14:23:19 +0100

    relation + location methode fini

--- models/location.go
+++ models/location.go
@@ -1,5 +1,7 @@
 package models
 
+import "strings"
+
 type Location struct {
     Id      int      `json:"id"`
     City    string   `json:"city"`
@@ -9,3 +11,15 @@ type Location struct {
 func (l *Location) HasLocation(loc string) bool {
     return l.City == loc
 }
+
+func (l *Location) MatchesQuery(query string) bool {
+    q := strings.ToLower(query)          
+    city := strings.ToLower(l.City)      
+    country := strings.ToLower(l.Country)
+
+    if strings.Contains(city, q) || strings.Contains(country, q){
+        return true
+    }
+    return false
+}
+
--- models/relation.go
+++ models/relation.go
@@ -5,3 +5,15 @@ type Relation struct {
     DateId     int `json:"dateId"`
     LocationId int `json:"locationId"`
 }
+
+func (r *Relation) MatchesArtist(id int) bool {
+	return id == r.ArtistId
+}
+
+func (r *Relation) MatchesDate(id int) bool {
+	return id == r.DateId
+}
+
+func (r *Relation) MatchesLocation(id int) bool {
+	return id == r.LocationId
+}
